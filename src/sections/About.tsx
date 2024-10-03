81f3093581b6797305e7194d932452776525226e
Hunter Arbeeny <hntrbeeny@gmail.com>
2024-10-03 14:24:21 -0400

    Footer Links Implemented

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -95,91 +95,92 @@ const hobbies = [
 export const AboutSection = () => {
   const constraintRef = useRef(null);
   return (
-    <div className="py-20 lg:py-28">
-      <div className="container">
-        <SectionHeader
-          eyebrow="About Me"
-          title="A Glimpse Into My World"
-          description="Learn more about who I am, what I do, and what inspires me."
-        />
-        {/* TODO: Implement onclick function that brings you to a list of books or carousel of books */}
-        <div className="mt-20 flex flex-col gap-8">
-          <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
-            <Card className="h-[320px] md:col-span-2 lg:col-span-1">
-              <CardHeader
-                title="My Reads"
-                description="Explore the books shaping my perspectives."
-              />
-              <div className="w-40 mx-auto mt-2 md:mt-0">
-                <Image src={bookImage} alt="Book Cover" />
-              </div>
-            </Card>
-            {/* TODO: Add icons of languages/frameworks i use */}
-            <Card className="h-[320px] md:col-span-3 lg:col-span-2">
-              <CardHeader
-                title="My Toolbox"
-                description="Explore the technologies and tools I use to craft exceptional
-                digital experiences."
-                className=""
-              />
-              <ToolboxItems
-                items={toolboxItems}
-                className="animate-move-left [animation-duration:30s]"
-              />
-              <ToolboxItems
-                items={toolboxItems}
-                className="mt-6"
-                itemsWrapper="-translate-x-1/2 animate-move-right [animation-duration:15s]"
-              />
-            </Card>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
-            <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
-              <CardHeader
-                title="Beyond the Code"
-                description="Explore my interests and hobbies beyond the digital realm."
-                className="px-6 py-6"
-              />
-              <div className="relative flex-1" ref = {constraintRef}>
-                
-                {hobbies.map((hobby) => (
-                  <motion.div
-                    key={hobby.title}
-                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
-                    style={{
-                      left: hobby.left,
-                      top: hobby.top,
-                    }}
-                    drag
-                    dragConstraints = {constraintRef}
-                  >
-                    <span className="font-medium text-gray-950">
-                      {hobby.title}
-                    </span>
-                    <span className="">{hobby.emoji}</span>
-                  </motion.div>
-                ))}
-              </div>
-            </Card>
-            <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
-              <Image
-                src={mapImage}
-                alt="map"
-                className="h-full w-full object-cover"
-              />
-              <div className="absolute top-1/2 left-1/2 -translate-x-1 translate-y-1 size-12 rounded-full  after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
-                <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
-                <Image
-                  src={smileMemoji}
-                  alt="smiling memoji"
-                  className="size-12"
+    <section id="about">
+      <div className="py-20 lg:py-28">
+        <div className="container">
+          <SectionHeader
+            eyebrow="About Me"
+            title="A Glimpse Into My World"
+            description="Learn more about who I am, what I do, and what inspires me."
+          />
+          {/* TODO: Implement onclick function that brings you to a list of books or carousel of books */}
+          <div className="mt-20 flex flex-col gap-8">
+            <div className="grid grid-cols-1 gap-8 md:grid-cols-5 lg:grid-cols-3">
+              <Card className="h-[320px] md:col-span-2 lg:col-span-1">
+                <CardHeader
+                  title="My Reads"
+                  description="Explore the books shaping my perspectives."
                 />
-              </div>
-            </Card>
+                <div className="w-40 mx-auto mt-2 md:mt-0">
+                  <Image src={bookImage} alt="Book Cover" />
+                </div>
+              </Card>
+              {/* TODO: Add icons of languages/frameworks i use */}
+              <Card className="h-[320px] md:col-span-3 lg:col-span-2">
+                <CardHeader
+                  title="My Toolbox"
+                  description="Explore the technologies and tools I use to craft exceptional
+                digital experiences."
+                  className=""
+                />
+                <ToolboxItems
+                  items={toolboxItems}
+                  className="animate-move-left [animation-duration:30s]"
+                />
+                <ToolboxItems
+                  items={toolboxItems}
+                  className="mt-6"
+                  itemsWrapper="-translate-x-1/2 animate-move-right [animation-duration:15s]"
+                />
+              </Card>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-3 gap-8">
+              <Card className="h-[320px] p-0 flex flex-col md:col-span-3 lg:col-span-2">
+                <CardHeader
+                  title="Beyond the Code"
+                  description="Explore my interests and hobbies beyond the digital realm."
+                  className="px-6 py-6"
+                />
+                <div className="relative flex-1" ref={constraintRef}>
+                  {hobbies.map((hobby) => (
+                    <motion.div
+                      key={hobby.title}
+                      className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                      style={{
+                        left: hobby.left,
+                        top: hobby.top,
+                      }}
+                      drag
+                      dragConstraints={constraintRef}
+                    >
+                      <span className="font-medium text-gray-950">
+                        {hobby.title}
+                      </span>
+                      <span className="">{hobby.emoji}</span>
+                    </motion.div>
+                  ))}
+                </div>
+              </Card>
+              <Card className="h-[320px] p-0 relative md:col-span-2 lg:col-span-1">
+                <Image
+                  src={mapImage}
+                  alt="map"
+                  className="h-full w-full object-cover"
+                />
+                <div className="absolute top-1/2 left-1/2 -translate-x-1 translate-y-1 size-12 rounded-full  after:content-[''] after:absolute after:inset-0 after:outline after:outline-2 after:-outline-offset-2 after:rounded-full after:outline-gray-950/30">
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-10"></div>
+                  <div className="absolute inset-0 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 -z-20 animate-ping [animation-duration:2s]"></div>
+                  <Image
+                    src={smileMemoji}
+                    alt="smiling memoji"
+                    className="size-12"
+                  />
+                </div>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
